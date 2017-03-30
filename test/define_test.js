eaf31a51cd2d0b013c6bd630c6bcd69ac634952d
Taka Okunishi <okunishinish@gmail.com>
2017-03-30 17:39:43 +0900

    Add watch tasks

--- test/define_test.js
+++ test/define_test.js
@@ -7,10 +7,12 @@
 const define = require('../lib/define.js')
 const ponContext = require('pon-context')
 const { ok } = require('assert')
+const asleep = require('asleep')
+const writeout = require('writeout')
 const co = require('co')
 
 describe('define', function () {
-  this.timeout(3000)
+  this.timeout(5000)
 
   before(() => co(function * () {
 
@@ -30,6 +32,21 @@ describe('define', function () {
     ok(task)
 
     yield Promise.resolve(task(ctx))
+  }))
+
+  it('Watch', () => co(function * () {
+    let ctx = ponContext({})
+    let srcDir = `${__dirname}/../tmp/testing-watching/src`
+    let destDir = `${__dirname}/../tmp/testing-watching/dest`
+    let src = srcDir + '/foo.pcss'
+    yield writeout(src, ':root { --red: #d33; } a { &:hover { color: color(var(--red) a(54%)); } }', { mkdirp: true })
+    yield asleep(100)
+    let close = yield define(srcDir, destDir, { watchDelay: 1 }).watch(ctx)
+    yield writeout(src, ':root { --red: #dd1; } a { &:hover { color: color(var(--red) a(54%)); } }', { mkdirp: true })
+    yield asleep(200)
+    yield writeout(src, ':root { --red: #5FF; } a { &:hover { color: color(var(--red) a(54%)); } }', { mkdirp: true })
+    yield asleep(200)
+    yield close()
   }))
 })
 
